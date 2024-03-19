a5d793413b587ba9d4bfec8c6266386d2b3866fa
AlanKevin2077 <2901449366@qq.com>
2024-03-19 09:33:10 +0800

    update

--- Java实例/其他实例/Soldier.java
+++ Java实例/其他实例/Soldier.java
@@ -118,3 +118,95 @@ public class Main {
     }
 }
             
+
+
+
+              //例子3
+              
+/*在这个设计中，我们定义了一个Weapon接口，它规定了武器必须实现的fire和reload方法。AK47类实现了这个接口，提供了具体的实现。
+
+Soldier类有一个Weapon类型的武器属性，以及advance、attack、reloadWeapon和setWeapon方法。setWeapon方法允许士兵更换他们的武器。
+
+在Main类的测试代码中，我们创建了一个Soldier对象，并调用了它的前进、进攻、装载子弹和更换武器的方法来演示这些功能。
+
+这个设计更加灵活和可扩展，你可以根据需要添加更多的武器类型，并且通过实现Weapon接口来创建新的武器类。同时，士兵类可以通过setWeapon方法轻松地更换武器，这为士兵提供了更多的战术选择。*/
+
+// 武器接口
+interface Weapon {
+    void fire();
+    void reload(int bullets);
+}
+
+// 具体武器类：AK47，创建一个名为 AK47 的类，并且让这个类实现 Weapon接口
+class AK47 implements Weapon {  
+    private int ammo;
+
+    public AK47() {
+        this.ammo = 30; // 默认子弹数量
+    }
+
+    @Override
+    public void fire() {
+        if (ammo > 0) {
+            ammo--;
+            System.out.println("AK47 发射子弹！剩余子弹：" + ammo);
+        } else {
+            System.out.println("AK47 没有子弹了！");
+        }
+    }
+
+    @Override
+    public void reload(int bullets) {
+        ammo += bullets;
+        System.out.println("AK47 装载子弹！当前子弹数量：" + ammo);
+    }
+}
+
+// 士兵类
+class Soldier {
+    private String name;
+    private Weapon weapon;
+
+    public Soldier(String name) {
+        this.name = name;
+        this.weapon = new AK47(); // 士兵初始装备AK47
+    }
+
+    public void advance() {
+        System.out.println(name + " 正在前进...");
+    }
+
+    public void attack() {
+        weapon.fire(); // 士兵使用武器发射子弹
+    }
+
+    public void reloadWeapon(int bullets) {
+        weapon.reload(bullets); // 士兵装载子弹到武器
+    }
+
+    public void setWeapon(Weapon newWeapon) {
+        this.weapon = newWeapon; // 更换士兵的武器
+    }
+}
+
+// 测试代码
+public class Main {
+    public static void main(String[] args) {
+        Soldier soldier = new Soldier("士兵1");
+
+        // 士兵前进
+        soldier.advance();
+
+        // 士兵进攻，发射子弹
+        soldier.attack();
+
+        // 士兵装载子弹
+        soldier.reloadWeapon(10);
+
+        // 更换士兵的武器
+        soldier.setWeapon(new AK47());
+
+        // 再次士兵进攻，发射子弹
+        soldier.attack();
+    }
+}
